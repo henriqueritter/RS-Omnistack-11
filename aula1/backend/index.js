2ae5398aac5b41b51acc7046beff4bb496a30bdf
henriqueritter <henrique-ritter@hotmail.com>
2020-03-24 23:31:28 -0300

    app on index.js

--- aula1/backend/index.js
+++ aula1/backend/index.js
@@ -4,7 +4,9 @@ const express = require('express');
 //Variavel que vai armazenar a minha aplicação.
 const app = express();
 
-
+app.get('/', (request, response) => {
+    return response.send('Hello World!');
+});
 
 //faz a aplicação escutar na porta 3333
 app.listen(3333);
