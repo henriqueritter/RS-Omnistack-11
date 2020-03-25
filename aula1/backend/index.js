9f89fd9f32a75ee4a1bdd631812f44fbc80131f7
henriqueritter <henrique-ritter@hotmail.com>
2020-03-25 00:07:26 -0300

    entendendo react

--- aula1/backend/index.js
+++ aula1/backend/index.js
@@ -5,7 +5,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.send('Hello World!');
+    //return response.send('Hello World!'); //tambem podemos retornar a resposta em formato JSON
+    return response.json({ 
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Henrique Ritter'
+    });
 });
 
 //faz a aplicação escutar na porta 3333
