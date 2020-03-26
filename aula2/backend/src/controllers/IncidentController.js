05b80717cdd14d21b735a56e4909d060c2d2f743
henriqueritter <henrique-ritter@hotmail.com>
2020-03-26 20:52:10 -0300

    aula 2 finalizada; instalado cors; criado sessao de login; back end base criado

--- aula2/backend/src/controllers/IncidentController.js
+++ aula2/backend/src/controllers/IncidentController.js
@@ -3,7 +3,27 @@ const connection = require('../database/connection');
 module.exports = {
     /** Rota de consulta */
     async index(request, response){
-        const incidents = await connection('incidents').select('*');
+        
+        /** Retorna quantidade total de registros, para auxilar no calculo de paginas necessárias */
+        const { page = 1 } = request.query;   //inicia o query parm das paginas se não houver nada, sera como 1
+                const [count] = await connection('incidents')   //Retorna a quantidade de registros 
+            .count();                                           //de incidents cadastrados
+                               //A Constante foi definida entre colchetes para exiber apenas um registro(o que no caso ela só traria um mesmo)
+        //linha abaixo: retorna a quantidade total de registros dentro do HEADERS em uma propriedade chmada X-Total-Count(escolhemos o nome)        
+        response.header('X-Total-Count', count['count(*)']); //acessamos somente o valor da variavel/objeto count no valor da propriedade 'count(*)'
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id') //realiza o join da tabela ONGS onde o campo ID da table ONG seja igual ao campo ONG_ID da table INCIDENTS
+            .limit(5)       //retorna apenas 5 incidents
+            .offset((page -1) * 5)  //isso fara com que os registros sejam retornados de 5 em cinco de acordo com a pagina
+            .select([               //cada pagina fica http://localhost:3333/incidents?page=2 e etc.. (A PRIMEIRA NAO PRECISA E PARAMETRO)
+                'incidents.*', 
+                'ongs.name',        //aqui declaramos como uma array os campos a serem pesquisados
+                'ongs.email', 
+                'ongs.whatsapp', 
+                'ongs.city', 
+                'ongs.uf'
+            ]);           
         return response.json(incidents);
     },
     /** Rota de criar incidents(casos) */
@@ -32,7 +52,6 @@ module.exports = {
             .select('ong_id')                   //e retorna a coluna ong_id apenas
             .first();                   //first() retorna o primeiro resultado(no nosso caso só existira uma unica ocorrencia com a ID do incident
                                         // ja que ela é feita de forma incremental) fazendo isso NAO RETORNA UMA ARRAY.
-        console.log('asdte');
         if (incident.ong_id != ong_id) {   //se o id da ong(do header.authorization) for diferente da cadastrada  ele retorna uma mensagem 401
             return response.status(401).json({ error: 'Operation Not Permitted. '}); // modifica o status do HTTP para NAO Autorizado
         }
--- aula2/backend/src/index.js
+++ aula2/backend/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');  
-
+const cors = require('cors');
 
 const routes = require('./routes');  //é necessario colocar ./  para o NODE entender que se trata de um arquivo e não um pacote, se quiser voltar uma pasta coloque ../
 
 
 const app = express();
 
+app.use(cors(
+    //se o app estivesse em producao poderiamos colocar o parametro abaixo
+    //origin: 'http://meusiteHospedadoQuePodeAcessar.meuapp.com
+    //porem como estamos no develop, só de declarar ele aqui sem parametro ja conseguiremos acessar este backend por qualquer 
+    //front end aqui
+));
 app.use(express.json()); //antes de todas as requisições o express vai converter os textos em JSON para ser entendido pelo App 
 app.use(routes);
 
