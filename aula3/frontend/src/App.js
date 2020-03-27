e50db62310a0b1378290bd4598fa1405528c31b3
henriqueritter <henrique-ritter@hotmail.com>
2020-03-27 10:17:57 -0300

    started page create

--- aula3/frontend/src/App.js
+++ aula3/frontend/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+//JSX (Javascript XML)
+import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  //Array do useState [ valorVariavel, funcaoDeAtualizacao ]
+
+  function increment(){
+    setCounter(counter + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack 11.0!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>Contador: {counter} </Header>
+      <button onClick={increment}>Incrementar</button>
     </div>
   );
 }
+
 
 export default App;
--- aula3/frontend/src/index.js
+++ aula3/frontend/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App';  //ele esta importando o arquivo App.js da pasta SRC(mesma pasta)
 
-ReactDOM.render(
+ReactDOM.render(    //colocando em tela
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />           
+  </React.StrictMode>,  //por App ser um componente podemos escrever ele como uma tag HTML como uma DIV ou outro
+  document.getElementById('root')   //colcando onde a ID for root
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
